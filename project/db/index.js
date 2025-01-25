7996a727f17ce8fb6b21285003c428103f24ea8c
AthmaniAmine <a_athmani@estin.dz>
2025-01-25 22:41:49 +0100

    init

--- project/db/index.js
+++ project/db/index.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Set to true for production with proper SSL certificates
+  },
 });
 
 module.exports = pool;
